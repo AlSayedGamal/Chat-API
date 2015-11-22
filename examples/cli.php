ccfe7ea1f56445820308c789e8fc9b46fade93d4
AlSayed Gamal <mail.gamal@gmail.com>
2015-11-22 09:47:17 +0200

    added login option

--- examples/cli.php
+++ examples/cli.php
@@ -5,15 +5,13 @@ require_once '../src/Registration.php';
 $debug    = DEBUG;  // Shows debug log, this is set to false if not specified
 $log      = LOGGING;  // Enables log file, this is set to false if not specified
 if ($argv == null){
-  echo "Usage: PHP {$argv[0]} <number> <nickname>";
+  echo "Usage: PHP {$argv[0]} --option arg1 arg2...";
 }
 var_dump($argv);
 switch ($argv[1]) {
   case '--register':
     $username = $argv[2];    // Your number with country code, ie: 34123456789
     $nickname = $argv[3];    // Your nickname, it will appear in push notifications
-    $w = new WhatsProt($username, $nickname, $debug, $log);
-    $w = new WhatsProt($username, $nickname, $debug, $log);
     $r = new Registration($username, $debug);
     $r->codeRequest('sms');
     echo "You must have received and SMS with your code save it for next step";
@@ -23,7 +21,16 @@ switch ($argv[1]) {
     $code = $argv[3];
     $r = new Registration($username, $debug);
     $r->codeRegister($code);
-    echo "your code has been registered";
+    echo "your code has been registered .. save the password for next step";
+    break;
+  case '--login-user':
+    $username = $argv[2];    // Your number with country code, ie: 34123456789
+    $nickname = $argv[3];    // Your nickname, it will appear in push notifications
+    $password = $argv[4];    // Your nickname, it will appear in push notifications
+    $w = new WhatsProt($username, $nickname, $debug);
+    $w->connect(); // Connect to WhatsApp network
+    $w->loginWithPassword($password); // logging in with the password we got!
+    echo "your code has been registered .. save the password for next step";
     break;
   default:
     echo "Please, choose one of the options --register --login";
