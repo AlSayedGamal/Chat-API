1673d2848347c685b27ea512cdb71b7562d93dd4
AlSayed Gamal <mail.gamal@gmail.com>
2015-11-24 10:54:13 +0200

    json responses

--- examples/cli.php
+++ examples/cli.php
@@ -7,16 +7,16 @@ $debug    = DEBUG;  // Shows debug log, this is set to false if not specified
 $log      = LOGGING;  // Enables log file, this is set to false if not specified
 if (isset($argv[1]) == false){
   echo "Usage:  PHP cli.php [option] arg1 arg2...
---register          --register <username>
-                    --register 20123456789
+".bold("--register")."      --register <username>
+                --register 20123456789
 
---register-code     --register-code <username> <SMS-code-withouth-dash>
-                    --register-code 20123456789 123456
+".bold("--register-code")." --register-code <username> <SMS-code-withouth-dash>
+                --register-code 20123456789 123456
 
---login-user        --login-user <username> <nickname> <password>
-                    --login-user 20123456789 AlSayedGamal i3u4o23i4b234goi4u23l4kjblk34
---msg-text          --msg-text <username> <nickname> <password> <destination> <msg>
-                    --msg-text 20123456789 AlSayedGamal i3u4o23i4b234goi4u23l4kjblk34
+".bold("--login-user")."    --login-user <username> <nickname> <password>
+                            --login-user 20123456789 AlSayedGamal i3u4o23i4b234goi4u23l4kjblk34
+".bold("--msg-text")."      --msg-text <username> <nickname> <password> <destination> <msg>
+                --msg-text 20123456789 AlSayedGamal i3u4o23i4b234goi4u23l4kjblk34
   ";
 }
 if (isset($argv[1])){
@@ -24,16 +24,19 @@ if (isset($argv[1])){
       case '--register':
         $username = $argv[2];    // Your number with country code, ie: 34123456789
         $r = new Registration($username, $debug);
-        $r->codeRequest('sms');
-        echo "You must have received and SMS with your code save it for next step";
+        try {
+            $res = $r->codeRequest('sms');
+        } catch (Exception $e) {
+            echo json_encode($e);
+        }
+        echo $res;
         break;
       case '--register-code':
         $username = $argv[2];    // Your number with country code, ie: 34123456789
         $code = $argv[3];
         $r = new Registration($username, $debug);
-        var_dump($r->codeRegister($code));
-
-        echo "your code has been registered .. save the password for next step";
+        $res = json_encode($r->codeRegister($code));
+        echo $res ;
         break;
       case '--login-user':
         $username = $argv[2];    // Your number with country code, ie: 34123456789
@@ -42,7 +45,6 @@ if (isset($argv[1])){
         $w = new WhatsProt($username, $nickname, $debug);
         $w->connect(); // Connect to WhatsApp network
         $w->loginWithPassword($password); // logging in with the password we got!
-        echo "your code has been registered .. save the password for next step";
         break;
       case '--msg-text':
         $src = $argv[2];    // Your number with country code, ie: 201003544877
@@ -54,7 +56,8 @@ if (isset($argv[1])){
             $w = new WhatsProt($src, $nickname, $debug);
             $w->connect(); // Connect to WhatsApp network
             $w->loginWithPassword($password); // logging in with the password we got!
-            $w->sendMessage($target , $msg);
+            echo json_encode($w->sendMessage($target , $msg));
+
         }else{
             echo "wrong parameters";
             echo "--msg-text          --msg-text <username> <nickname> <password> <destination> <msg>
